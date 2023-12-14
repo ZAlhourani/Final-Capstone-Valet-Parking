eb5260d7dec68c0874c39c75adcdf72e807d3002
Shaym12 <143984857+Shaym12@users.noreply.github.com>
2023-12-14 16:22:23 -0500

    blah

--- vue/src/views/CustomerParkingAvailability.vue
+++ vue/src/views/CustomerParkingAvailability.vue
@@ -4,7 +4,6 @@
     <p v-if="loading">Loading...</p>
     <div v-else>
   <p v-if="spaces.length > 0">We have {{ spaces.length }} spot{{ spaces.length === 1 ? '' : 's' }} available. <font-awesome-icon :icon="['far', 'face-smile']" /></p>
-      
       <div v-else class="alert alert-full">
         Lot is full
         <font-awesome-icon :icon="['far', 'face-frown']" />
@@ -12,15 +11,12 @@
     </div>
   </div>
 </template>
-
 <script>
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import ParkingSpotsService from '../services/ParkingSpotsService';
-
 library.add(far);
-
 export default {
   components: {
     'font-awesome-icon': FontAwesomeIcon
@@ -49,31 +45,26 @@ export default {
   }
 };
 </script>
-  
   <style>
 .font-awesome-icon {
   margin-left: 10px;
   color: white; /* Or any color that suits your alert */
 }
-
 .availability-container {
   text-align: center;
   margin-top: 20px;
-  font-family: 'Roboto', sans-serif; 
+  font-family: 'Roboto', sans-serif;
 }
-
 .availability-container h2 {
-  color: #bb29eb; 
-  text-shadow: 0 0 10px #bb29eb; 
+  color: #BB29EB;
+  text-shadow: 0 0 10px #BB29EB;
   font-size: 3rem;
   margin-bottom: 0.5em;
 }
-
 .availability-container p {
   color: White;
-  font-size: 35px; 
+  font-size: 35px;
 }
-
 .alert-full {
   color: white;
   background-color: red;
@@ -81,33 +72,24 @@ export default {
   margin: 20px 0;
   font-size: 35px;
   font-weight: bold;
-  border: none; 
-  border-radius: 5px; 
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
-  transition: background-color 0.3s; 
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s;
 }
-
 .alert-full:hover {
-  background-color: darkred; 
+  background-color: darkred;
 }
-
 @media (max-width: 768px) {
   .availability-container {
     margin-top: 10px;
   }
-
   .alert-full {
     padding: 15px;
     font-size: 18px;
   }
 }
-
 .spaces {
   font-size: 20px;
 }
-
-
-
-
   </style>
-  
